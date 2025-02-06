2244dc44f243573d04325ae8ae4a17b4adcf464a
Sora <sorasuzuki0427@gmail.com>
2025-02-06 14:17:30 +0800

    feat: initialize the day2 for rareskills tutorial: arithmetic and basic types in solana and rust

--- day2/programs/day2/src/lib.rs
+++ day2/programs/day2/src/lib.rs
@@ -1,12 +1,12 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Af2eZdMRUFKLeG4nuv5uUJUPQFxME5tUEjHm3zrbAKan");
+declare_id!("wVZkGRvX27oEyomZNyCEWJYjK4zk1kMqrBQVDgRK2Wh");
 
 #[program]
-pub mod day1 {
+pub mod day2 {
     use super::*;
 
-    pub fn initialize2(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
--- day2/tests/day2.ts
+++ day2/tests/day2.ts
@@ -1,16 +1,16 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Day1 } from "../target/types/day1";
+import { Day2 } from "../target/types/day2";
 
-describe("day1", () => {
+describe("day2", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Day1 as Program<Day1>;
+  const program = anchor.workspace.Day2 as Program<Day2>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize2().rpc();
+    const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
 });
