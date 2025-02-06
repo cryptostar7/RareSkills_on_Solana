d1f3a35198e12a3a9c5379091adeae8e5e6fa05a
Sora <sorasuzuki0427@gmail.com>
2025-02-06 15:14:57 +0800

    feat: completed the day2

--- day2/programs/day2/src/lib.rs
+++ day2/programs/day2/src/lib.rs
@@ -6,8 +6,18 @@ declare_id!("wVZkGRvX27oEyomZNyCEWJYjK4zk1kMqrBQVDgRK2Wh");
 pub mod day2 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, a: u64, b: u64, message: String) -> Result<()> {
+        msg!("You sent {} and {}", a, b);
+        msg!("You said: {:?}", message);
+        let x: u64 = 2;
+        let y = 3;
+        let result = x.pow(y);
+        msg!("The result is {}", result);
+        Ok(())
+    }
+
+    pub fn array(ctx: Context<Initialize>, arr: Vec<u64>) -> Result<()> {
+        msg!("Your array {:?}", arr);
         Ok(())
     }
 }
