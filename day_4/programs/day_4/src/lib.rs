6e2de91df22f289b7568a63fd40aae9f9a703aec
Sora <sorasuzuki0427@gmail.com>
2025-02-09 01:03:15 +0800

    feat: complete the day4: errors

--- day_4/programs/day_4/src/lib.rs
+++ day_4/programs/day_4/src/lib.rs
@@ -2,15 +2,73 @@ use anchor_lang::prelude::*;
 
 declare_id!("3u5QzuRq3sNNb34rX1Ki99DUPthmh3WP9PhExqu28nfA");
 
+// // Using the err! statement
+// #[program]
+// pub mod day_4 {
+//     use super::*;
+
+//     pub fn limit_range(ctx: Context<LimitRange>, a: u64) -> Result<()> {
+//         if a < 10 {
+//             return err!(MyError::AisTooSmall);
+//         }
+//         if a > 100 {
+//             return err!(MyError::AisTooBig);
+//         }
+
+//         msg!("Result = {}", a);
+//         Ok(())
+//     }
+
+//     pub fn func(ctx: Context<LimitRange>) -> Result<()> {
+//         msg!("Will this print?");
+//         return err!(MyError::AlwaysErrors);
+//     }
+// }
+
+// #[derive(Accounts)]
+// pub struct LimitRange {}
+
+// #[error_code]
+// pub enum MyError {
+//     #[msg("a is too big")]
+//     AisTooBig,
+//     #[msg("a is too small")]
+//     AisTooSmall,
+//     #[msg("AlwaysErrors")]
+//     AlwaysErrors,
+// }
+
+/**
+ * @title : Using the require statement
+ */
+
 #[program]
 pub mod day_4 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn limit_range(ctx: Context<ReturnError>, a: u64) -> Result<()> {
+        require!(a >= 10, Day4Error::AisTooSmall);
+        require!(a <= 100, Day4Error::AisTooBig);
+
+        msg!("Result = {}", a);
         Ok(())
+    }
+
+    pub fn func(ctx: Context<ReturnError>) -> Result<()> {
+        msg!("Will this print?");
+        return err!(Day4Error::AlwaysErrors);
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct ReturnError {}
+
+#[error_code]
+pub enum Day4Error {
+    #[msg("a is too small")]
+    AisTooSmall,
+    #[msg("a is too big")]
+    AisTooBig,
+    #[msg("AlwaysErrors")]
+    AlwaysErrors,
+}
--- day_4/tests/day_4.ts
+++ day_4/tests/day_4.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Day4 } from "../target/types/day_4";
+import { assert } from "chai";
 
 describe("day_4", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,41 @@ describe("day_4", () => {
 
   const program = anchor.workspace.Day4 as Program<Day4>;
 
-  it("Is initialized!", async () => {
+  it("Input Test", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    try {
+      const tx = await program.methods.limitRange(new anchor.BN(9)).rpc();
+      console.log("Your transaction signature", tx);  
+    } catch (_err) {
+      assert.isTrue(_err instanceof anchor.AnchorError);
+      const err: anchor.AnchorError = _err;
+      const errMsg = "a is too small";
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log("Error number:", err.error.errorCode.number);
+    }
+
+    try {
+      const tx = await program.methods.limitRange(new anchor.BN(101)).rpc();
+      console.log("Your transaction signature", tx);
+    } catch(_err) {
+      assert.isTrue(_err instanceof anchor.AnchorError);
+      const err: anchor.AnchorError = _err;
+      const errMsg = "a is too big";
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log("Error number:", err.error.errorCode.number);
+    }
   });
+
+  it("Error test", async () => {
+    try {
+      const tx = await program.methods.func().rpc();
+      console.log("Your transaction signature", tx);
+    } catch (_err) {
+      assert.isTrue(_err instanceof anchor.AnchorError);
+      const err: anchor.AnchorError = _err;
+      const errMsg = "AlwaysErrors";
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log("Error number:", err.error.errorCode.number);
+    }
+  })
 });
