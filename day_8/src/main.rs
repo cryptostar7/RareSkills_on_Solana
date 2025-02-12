46730d41212bd097d49eab93dc735bc20cd3a806
Sora <sorasuzuki0427@gmail.com>
2025-02-12 13:58:42 +0800

    feat: complete the day_8 course

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -1,3 +1,7 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    std::io::stdout().write(b"1n").unwrap();
+    std::io::stdout().write(b"1n", b"2\n").unwrap();
+    std::io::stdout().write(b"1n", b"2\n", b"3\n").unwrap();
 }
