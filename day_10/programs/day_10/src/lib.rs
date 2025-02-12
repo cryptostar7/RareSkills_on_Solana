05936f6ef36a0ddc633bfd6f43449038f77d38bd
Sora <sorasuzuki0427@gmail.com>
2025-02-12 17:26:35 +0800

    feat: initialize the day_10: visibility

---
+++ day_10/programs/day_10/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("9mAj7689DFavC2rnz7U4wCG9oeH59qWrZSEQDPE7TRtU");
+
+#[program]
+pub mod day_10 {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
