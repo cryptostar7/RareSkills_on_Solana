94d0bd54331d1eb257bd1fafe1b53f1f68049512
Sora <sorasuzuki0427@gmail.com>
2025-02-20 15:33:40 +0800

    feat: complete the day_14

--- day_14/programs/day_14/src/lib.rs
+++ day_14/programs/day_14/src/lib.rs
@@ -1,16 +1,68 @@
 use anchor_lang::prelude::*;
 
 declare_id!("7diQito7iXkZe5Y3udC9bT275UhHJfqyc34R7AbqH1zN");
+/**
+ * Signer Patter
+ */
+
+// #[program]
+// pub mod day_14 {
+//     use super::*;
+
+//     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+//         let the_signer1: &mut Signer = &mut ctx.accounts.signer1;
+//         let the_signer2: &mut Signer = &mut ctx.accounts.signer2;
+
+//         msg!("The signer1: {:?}", *the_signer1.key);
+//         msg!("The signer2: {:?}", *the_signer2.key);
+
+//         Ok(())
+//     }
+// }
+
+
+// #[derive(Accounts)]
+// pub struct Initialize<'info> {
+//     pub signer1: Signer<'info>,
+//     pub signer2: Signer<'info>,
+// }
+
+
+/**
+ * Only Owner Patter
+ */
+
+const OWNER: &str = "8os8PKYmeVjU1mmwHZZNTEv5hpBXi5VvEKGzykduZAik";
 
 #[program]
 pub mod day_14 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    #[access_control(check(&ctx))]
+    pub fn initialize(ctx: Context<OnlyOwner>) -> Result<()> {
+        msg!("Hello, I am owner");
         Ok(())
     }
 }
 
+fn check(ctx: &Context<OnlyOwner>) -> Result<()> {
+    require_keys_eq!(
+        ctx.accounts.signer.key(),
+        OWNER.parse::<Pubkey>().unwrap(),
+        OnlyOwnerError::NotOwner
+    );
+
+    Ok(())
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct OnlyOwner<'info> {
+    signer_account: Signer<'info>,
+}
+
+
+#[error_code]
+pub enum OnlyOwnerError {
+    #[msg("Only owner can call this function")]
+    NotOwner,
+}
--- day_14/tests/day_14.ts
+++ day_14/tests/day_14.ts
@@ -8,9 +8,20 @@ describe("day_14", () => {
 
   const program = anchor.workspace.Day14 as Program<Day14>;
 
+  let myKeypair = anchor.web3.Keypair.generate();
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods
+    .initialize()
+    .accounts({
+      signer1: program.provider.publicKey,
+      signer2: myKeypair.publicKey,
+    })
+    .signers([myKeypair])
+    .rpc();
+
+  console.log("The signer1: ", program.provider.publicKey.toBase58());
+  console.log("The signer2: ", myKeypair.publicKey.toBase58());
   });
 });
