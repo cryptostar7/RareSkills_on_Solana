9bc88cb8b5f994dc2c088e46447dd3aaa5e1791c
Sora <sorasuzuki0427@gmail.com>
2025-02-21 15:43:58 +0800

    feat: complete the token mint process

--- Midterm Exam/transfer/programs/transfer/src/lib.rs
+++ Midterm Exam/transfer/programs/transfer/src/lib.rs
@@ -1,4 +1,10 @@
-use anchor_lang::prelude::*;
+use {
+    anchor_lang::prelude::*,
+    anchor_spl::{
+        associated_token::AssociatedToken,
+        token::{transfer, Mint, Token, TokenAccount, Transfer},
+    },
+};
 
 declare_id!("7iFDug1T87BetkqUDAEakFktt8UvKDGpSnS3ZNhtLJqF");
 
@@ -6,11 +12,39 @@ declare_id!("7iFDug1T87BetkqUDAEakFktt8UvKDGpSnS3ZNhtLJqF");
 pub mod transfer {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn transfer(ctx: Context<TransferTokens>) -> Result<()> {
+        msg!("Transferring tokens...");
+        msg!("Mint: {}", &ctx.accounts.mint_account.to_account_info().key());
+        msg!("From Token Address: {}", &ctx.accounts.sender_token_account.key())
+        msg!("To Token Address: {}", &ctx.accounts.recipient)
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct TransferTokens<'info> {
+    #[account(mut)]
+    pub sender: Signer<'info>,
+    pub recipient: SystemAccount<'info>,
+
+    #[account(mut)]
+    pub mint_account: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_account,
+        associated_token::authority = sender,
+    )]
+    pub sender_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = sender,
+        associated_token::mint = mint_account,
+        associated_token::authority = recipient,
+    )]
+    pub recipient_token_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
